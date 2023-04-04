9ab8a4144349e6406cd3e45b9cb7fc4fbf1efaa8
Choi-HyunHo <x7788@naver.com>
2023-04-04 14:15:12 +0900

    style : tailwind css 세팅 및 Navbar 스타일링

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Home from "./pages/Home";
 
 function App() {
-	return <>11</>;
+	return (
+		<>
+			<Home />
+		</>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{js,jsx}"],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brand: "#F96162",
+			},
+		},
 	},
 	plugins: [],
 };
