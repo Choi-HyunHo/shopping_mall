87b441100643457e8f1ca28c10b015006a3e1485
Choi-HyunHo <x7788@naver.com>
2023-04-04 15:49:23 +0900

    feat : firebase 구글 로그인 적용

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
 
+// 구글 로그인
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../fbase";
+
 const Navbar = () => {
+	const [userData, setUserData] = useState();
+
+	const handleGoogleLogin = () => {
+		const provider = new GoogleAuthProvider(); // provider를 구글로 설정
+		signInWithPopup(auth, provider) // popuop을 이용한 signUp
+			.then((data) => {
+				setUserData(data.user);
+				console.log(data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	return (
 		<header className="flex justify-between border-b border-gray-300 p-2">
 			<Link to="/" className="flex items-center text-4xl text-brand">
@@ -16,7 +34,8 @@ const Navbar = () => {
 				<Link to="/products/new" className="text-2xl">
 					<BsFillPencilFill />
 				</Link>
-				<button>Login</button>
+				<button onClick={handleGoogleLogin}>Login</button>
+				{userData ? userData.displayName : null}
 			</nav>
 		</header>
 	);
