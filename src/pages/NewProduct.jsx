c7383188ddeb059fdb50030203ac396c95f3f4a0
Choi-HyunHo <x7788@naver.com>
2023-04-06 14:41:54 +0900

    style : 제품 등록 페이지 컴포넌트 큰 틀 및 스타일링

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const NewProduct = () => {
-	return <div>신 제품</div>;
+	return (
+		<div className="w-full text-center">
+			<h1 className="text-2xl font-bold my-4">새로운 제품 등록</h1>
+			<form className="flex flex-col px-12">
+				<input type="file" accept="image/*" name="file" required />
+				<input type="text" name="title" placeholder="제품명" required />
+				<input type="number" name="price" placeholder="가격" required />
+				<input
+					type="text"
+					name="category"
+					placeholder="카테고리"
+					required
+				/>
+				<input
+					type="text"
+					name="description"
+					placeholder="제품 설명"
+					required
+				/>
+				<input
+					type="text"
+					name="options"
+					placeholder="옵션들(콤마(,)로 구분)"
+					required
+				/>
+			</form>
+		</div>
+	);
 };
 
 export default NewProduct;
